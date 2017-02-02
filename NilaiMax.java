4b5233f7717d76370ff62d9e30de08900dca7ac9
muhammadrikiF <muhammadrikif@yahoo.com>
2017-02-02 08:28:46 +0700

    tambah fungsi cari nilai tertinggi dengan method kondisi ?:

--- NilaiMax.java
+++ NilaiMax.java
@@ -1,5 +1,17 @@
 public class NilaiMax {
 public static void main(String[]args){
+int a = 87, b = 17, c = 64;
+int max = 0;
+ 
+ System.out.println("number 1 = "+a);
+ System.out.println("number 2 = "+b);
+ System.out.println("number 3 = "+c);
+ 
+ //mencari nilai tertinggi dengan operator kondisi
+ max = a >= max ? a : max;
+ max = b >= max ? b : max;
+ max = c >= max ? c : max;
 
+ System.out.println("Nilai Tertingginya adalah angka = "+max);
 	}
 }
